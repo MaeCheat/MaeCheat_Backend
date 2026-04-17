7099ff0ed4d1e8e436314d7256cbe31665215528
shin1488 <syc1488@naver.com>
2026-04-17 15:02:58 +0900

    init commit

---
+++ src/main/java/dev/shin/maecheat/MaeCheatApplication.java
@@ -0,0 +1,13 @@
+package dev.shin.maecheat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaeCheatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaeCheatApplication.class, args);
+    }
+
+}
