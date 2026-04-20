7fbf659729bf1c605d58ddbe27bbbe37358d9c55
shin1488 <syc1488@naver.com>
2026-04-20 20:06:42 +0900

    변경 : cors 설정 global로 별도 작성 및 관련 코드 수정

--- src/main/java/dev/shin/maecheat/domain/character/controller/MapleCharacterController.java
+++ src/main/java/dev/shin/maecheat/domain/character/controller/MapleCharacterController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/characters")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
 public class MapleCharacterController {
     private final MapleCharacterService mapleCharacterService;
 
