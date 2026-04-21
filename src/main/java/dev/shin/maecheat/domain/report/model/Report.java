0e2328f87629448821669fb4046cd8238696c0e6
shin1488 <syc1488@naver.com>
2026-04-21 20:52:33 +0900

    추가 : report 엔티티 작성

---
+++ src/main/java/dev/shin/maecheat/domain/report/model/Report.java
@@ -0,0 +1,38 @@
+package dev.shin.maecheat.domain.report.model;
+
+import dev.shin.maecheat.domain.character.model.MapleCharacter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(
+        name = "report",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "UK_REPORT_SOURCE_URL", columnNames = {"source_url"})
+        }
+)
+public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String sourceUrl;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "maple_character_id", nullable = false)
+    private MapleCharacter mapleCharacter;
+}
