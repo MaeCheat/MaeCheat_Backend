00c2ff40191055efcb9fa2b76da9a75d2c1312e5
shin1488 <syc1488@naver.com>
2026-04-26 23:53:27 +0900

    추가 : report 레포지토리 작성

---
+++ src/main/java/dev/shin/maecheat/domain/report/repository/ReportRepository.java
@@ -0,0 +1,10 @@
+package dev.shin.maecheat.domain.report.repository;
+
+import dev.shin.maecheat.domain.report.model.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+
+}
