245f68f65f25e90b57352683262f633fbd825d58
shin1488 <syc1488@naver.com>
2026-04-30 10:47:40 +0900

    추가 : 메치트 도메인 CORS 허용

--- src/main/java/dev/shin/maecheat/global/config/CorsConfig.java
+++ src/main/java/dev/shin/maecheat/global/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://maecheat.vercel.app", "https://maecheat.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
